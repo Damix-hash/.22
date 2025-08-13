2f88d458a8c2ec5f3e0d6e0cfb4b6164fa211a27
Damix <158813145+Damix-hash@users.noreply.github.com>
2025-08-13 18:16:21 +0200

    Update events.js
    implemented bot uses

--- events.js
+++ events.js
@@ -167,6 +167,7 @@ module.exports = function(bot, state) {
 
                 if (public_commands.hasOwnProperty(cmd)) {
                     public_commands[cmd](username, command, bot, state)
+                    stats.bot_uses++
                 }
             }
         }
@@ -300,6 +301,7 @@ module.exports = function(bot, state) {
         }
     });
 };
+
 
 
 
