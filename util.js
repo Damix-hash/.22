55465509168806a6c928eebd0d03a8254d658ef5
itsinvoke <faizansecondiscord@gmail.com>
2025-08-19 15:14:15 +0530

    util.js

--- util.js
+++ util.js
@@ -7,8 +7,7 @@ let startTime = Date.now();
 const spam_count = {};
 const temp_blacklist = new Map();
 const spam_offenses = {};
-const whitelist = ['Damix2131', 'q33a', 'ryk_cbaool', '1nvoke_',
-    'Abottomlesspit', 'MioAutoCrystal', 'NIKASTEIN'];
+const whitelist = ['Damix2131', 'q33a', 'ryk_cbaool', '1nvoke_', 'MioAutoCrystal', 'NIKASTEIN'];
 
 async function updateUserLifetimeStats(username, state) {
   try {
@@ -297,5 +296,6 @@ module.exports = {
   spam_offenses,
   whitelist,
 };
+
 
 
